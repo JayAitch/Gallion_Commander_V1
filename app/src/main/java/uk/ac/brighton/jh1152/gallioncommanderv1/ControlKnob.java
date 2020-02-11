109dbbdcffed4eed6f80e51911450b13fb1e7436
Jordan <jordanjharrison@hotmail.co.uk>
2020-02-11 16:53:46 +0000

    addded new deffinition for control and label aim to take key up events and workout which stateit was cloest to this will avoid scroll manipulation shinanigans

--- app/src/main/java/uk/ac/brighton/jh1152/gallioncommanderv1/ControlKnob.java
+++ app/src/main/java/uk/ac/brighton/jh1152/gallioncommanderv1/ControlKnob.java
@@ -1,5 +1,11 @@
 package uk.ac.brighton.jh1152.gallioncommanderv1;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.text.StaticLayout;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +19,8 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.core.view.MotionEventCompat;
 
+import java.util.Map;
+
 public class ControlKnob extends LinearLayout implements ICustomControl {
 
     ImageView controlKnob;
@@ -20,7 +28,7 @@ public class ControlKnob extends LinearLayout implements ICustomControl {
     String name;
     String[] stateNames;
     int currentValue;
-
+    float angleOffset = 45;
 
     public ControlKnob(Context context) {
         super(context);
@@ -44,6 +52,29 @@ public class ControlKnob extends LinearLayout implements ICustomControl {
         setCurrentValue(currentValue);
     }
 
+    private void drawAllLabels(){
+        for(int stateLabelsInc = 0; stateLabelsInc < stateNames.length; stateLabelsInc++){
+        //    drawerTextAt(getStatePosition(stateLabelsInc), stateNames[stateLabelsInc]);
+        }
+}
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+        for(int stateLabelsInc = 0; stateLabelsInc < stateNames.length; stateLabelsInc++){
+            drawerTextAt(canvas, getStatePosition(stateLabelsInc), stateNames[stateLabelsInc]);
+        }
+    }
+
+    private void drawerTextAt(Canvas canvas,Point pos, String text){
+        Paint paint = new Paint();
+        paint.setTextSize(48);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(text, pos.x, pos.y, paint);
+        Log.d("new lavel", "at x:"+pos.x+"y:"+pos.y+text);
+    }
 
     private void createViewObjects(){
         LayoutInflater.from(getContext()).inflate(R.layout.control_knob, this);
@@ -51,14 +82,46 @@ public class ControlKnob extends LinearLayout implements ICustomControl {
         //stateText = (TextView) findViewById(R.id.stateText);
         controlKnob =(ImageView) findViewById(R.id.control_knob);
         nameText.setText(name);
+        drawAllLabels();
 
     }
 
     private void setRotation(int currentValue){
         float progress = ((float)currentValue / (float) stateNames.length);
-        float angle =  progress * 360;
+        float angle =  progress * 360 + angleOffset;
         controlKnob.setRotation(angle);
     }
+
+
+
+    private Point getStatePosition(int stateValue) {
+
+
+        float radius = (controlKnob.getWidth() / 2) - 20;
+        Double startAngle = Math.PI * (9 / 2d);
+        Double angle =  startAngle + (stateValue * ( (2 * Math.PI) / stateNames.length));
+
+        float xOrigin = controlKnob.getX() + (controlKnob.getWidth() / 2);
+        float yOrigin = controlKnob.getY() + (controlKnob.getHeight() / 2) + 20;
+
+        int posX  = (int) ((radius * Math.cos(angle)) + xOrigin);
+        int posY  = (int) ((radius * Math.sin(angle)) + yOrigin);
+        return new Point(posX, posY);
+    }
+
+
+    //https://gamedev.stackexchange.com/questions/9607/moving-an-object-in-a-circular-path
+//
+//    private Point getStatePosition(int stateValue){
+//
+//        float radius = controlKnob.getWidth() / 2;
+//        float angle = ((float)stateValue / (float) stateNames.length);
+//        float xOrigin = controlKnob.getX() + (controlKnob.getWidth() / 2);
+//        float yOrigin = controlKnob.getY() + (controlKnob.getHeight() / 2);
+//        int xPos = (int)(xOrigin + Math.cos((double) angle) * radius);
+//        int yPos = (int)(yOrigin + Math.sin((double) angle) * radius);
+//        return new Point(xPos,yPos);
+//    }
 
     private void snapToClosestState(){
         float currentRotation = controlKnob.getRotation();
@@ -94,7 +157,7 @@ public class ControlKnob extends LinearLayout implements ICustomControl {
         rotation = rotation + (amount * 5);
         controlKnob.setRotation(rotation % 360);
     }
-
+//https://www.pocketmagic.net/custom-rotary-knob-control-for-android/
     public void setCurrentValue(int statePosition){
     //    stateText.setText(stateNames[statePosition]);
     //    seekBar.setProgress(statePosition);
@@ -170,45 +233,12 @@ public class ControlKnob extends LinearLayout implements ICustomControl {
                 int action = event.getAction();
         switch (action) {
             case (MotionEvent.ACTION_DOWN):
-//                Log.d(DEBUG_TAG, "Action was DOWN");
-//                currentValue--;
-//                if(currentValue < 0){
-//                    currentValue = 0;
-//                }
-//                setCurrentValue(currentValue);
-                //getParent().requestDisallowInterceptTouchEvent(true);
+
                 return true;
             case (MotionEvent.ACTION_MOVE):
 
-
-
-                int historySize = event.getHistorySize();
-                float xd = 0;
-                float yd = 0;
-                float td = 0;
-                //float speed = 10;
-
-                if(historySize > 0) {
-                    xd = event.getX() - event.getHistoricalX(historySize - 1);
-                    yd = (event.getY() - event.getHistoricalY(historySize - 1)) * -1;
-                    td = xd + yd;
-                }
-                rotateKnob(td);
-                if(currentValue > stateNames.length){
-                   // currentValue = 0;
-                }
-             //   setCurrentValue(currentValue);
-                getParent().requestDisallowInterceptTouchEvent(true);
                 return true;
             case (MotionEvent.ACTION_UP):
-             //   snapToClosestState();
-//                Log.d(DEBUG_TAG, "Action was UP");
-//                currentValue++;
-//                if(currentValue > stateNames.length){
-//                    currentValue = 0;
-//                }
-//                setCurrentValue(currentValue);
-              //  getParent().requestDisallowInterceptTouchEvent(true);
                 return true;
             default:
                 return false;
